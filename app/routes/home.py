a22492f962e5aabd694361c91e2646551669278f
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-06 16:09:12 -0700

    added loggin validators

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -9,10 +9,11 @@ def index():
   # get all posts
   db = get_db()
   posts = db.query(Post).order_by(Post.created_at.desc()).all()
-
+  
   return render_template(
     'homepage.html',
-    posts=posts
+    posts=posts,
+    loggedIn=session.get('loggedIn')
   )
 
 @bp.route('/login')
@@ -28,5 +29,6 @@ def single(id):
   # render single post template
   return render_template(
     'single-post.html',
-    post=post
+    post=post,
+    loggedIn=session.get('loggedIn')
   )
